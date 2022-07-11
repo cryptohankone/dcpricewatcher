a251e06c4f6afb3f9495dc65b7280d320226174f
cryptohankone <cryptohankone@gmail.com>
2022-07-11 15:26:03 +0200

    Change to show all PegMarket tokens

--- main.js
+++ main.js
@@ -1,32 +1,45 @@
 const puppeteer = require("puppeteer");
 const notifier = require("node-notifier");
 
-const token = process.argv.slice(2);
-console.log(`Watching price for ${token}`);
+const SLEEP_MS = 3000;
+const TOKENS = ["DUSDT", "DUSDC", "DCRO", "DCOF"];
 
 (async () => {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(
-    `https://www.darkcrypto.finance/peg-market?sellToken=${token}`
-  );
-  await page.waitForSelector("div.box-price span");
+  const pages = {};
+  for (const token of TOKENS) {
+    const page = await browser.newPage();
+    await page.goto(
+      `https://www.darkcrypto.finance/peg-market?sellToken=${token}`
+    );
+    await page.waitForSelector("div.box-price span");
 
-  let sleepMs = 50;
+    pages[token] = page;
+  }
+
   while (true) {
-    const price = await page.$eval("div.box-price span", (el) => el.innerHTML);
-    if (!isNaN(price)) {
-      sleepMs = 3000;
-      if (price > 1.0) {
-        notifier.notify(`Price is ${price}`);
-      } else {
-        console.log(`Not yet.. ${price} < 1.0`);
-      }
-    } else {
-      console.log("Waiting for price..");
-    }
+    const prices = {};
+    for (const token of Object.keys(pages)) {
+      const page = pages[token];
+      let price = await page.$eval("div.box-price span", (el) => el.innerHTML);
 
-    await sleep(sleepMs);
+      if (!isNaN(price)) {
+        price = parseFloat(price);
+        if (price > 1.0) {
+          notifier.notify(`Price is for ${token} is above 1.0! ** ${price} **`);
+        }
+      } else {
+        price = "N/A";
+      }
+
+      prices[token] = price;
+    }
+    console.clear();
+    console.log(
+      `Watching prices for ${TOKENS.join(", ")} -- ${new Date().toDateString()}`
+    );
+    console.table(prices);
+    await sleep(SLEEP_MS);
   }
 
   await browser.close();
